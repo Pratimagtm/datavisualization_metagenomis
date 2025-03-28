fd479c0e06375ca446101f9a4c1eb2a4c0cf8f29
Pratimagtm <43498642+Pratimagtm@users.noreply.github.com>
2025-03-28 11:00:57 -0700

    update

--- figure2.R
+++ figure2.R
@@ -7,25 +7,22 @@ library(wesanderson)
 library(cowplot)
 
 
-excel_path <-("/home/pratima/Insync/pgautam1@umbc.edu/Google Drive/Cusick Lab/Metagenomic_analysis/Megan7_analysis/megan7_bacteria/figure123/genus.xlsx")
-#View(excel_path)
+excel_path <-("../genus.xlsx")
 
 sheet_data <- read.xlsx(excel_path, sheet = "genus", colNames = TRUE)
-# Data
+# Data with count for 2 different sample types
 data <- data.frame(
   Organisms <- sheet_data[1],
-  Biofilm <- sheet_data[2],
-  Water <- sheet_data[3]
+  Sample1 <- sheet_data[2],
+  Sample2 <- sheet_data[3]
 )
 
-#View(data)
 #change data to long format
 # Reshape the data into long format
-
 data_long <- data %>%
-  gather(key = "Condition", value = "Count", Biofilm, Water)
+  gather(key = "Condition", value = "Count", Sample1, Sample2)
 
-# Calculate total counts per condition (Biofilm, Water)
+# Calculate total counts per condition (Sample1, Sample2)
 total_counts <- data_long %>%
   group_by(Condition) %>%
   summarise(Total = sum(Count))
@@ -40,11 +37,9 @@ data_long <- data_long %>%
   group_by(Condition) %>%
   mutate(Organisms = fct_reorder(Organisms, Percentage, .desc = FALSE))
 
-View(data_long)
 # Create the stacked bar plot
 custom_colors <- colorRampPalette(RColorBrewer::brewer.pal(12, "Paired"))(271)
 p<- ggplot(data_long, aes(x = Condition, y = Percentage, fill = Organisms)) + 
-                      #xlim = c(0, 200), ylim = c(0, 100))) +
   geom_bar(stat = "identity") +
   scale_y_continuous(limits = c(0, NA), expand = c(0, 0)) +  # Y-axis starts at 0
   scale_x_discrete(expand = c(0, 0))+     # Ensure no expansion for x-axis
@@ -64,7 +59,6 @@ p<- ggplot(data_long, aes(x = Condition, y = Percentage, fill = Organisms)) +
 p2 <- ggplot(data_long, aes(x = Condition, y = Percentage, fill = Organisms)) +
   geom_bar(stat = "identity") +
   scale_fill_manual(values = custom_colors)+
-  #theme(legend.position = "right")  # Position legend on the right+
 theme(
   legend.text = element_text(size = 20),  # Correct use inside theme()
   legend.title = element_text(size = 20)
@@ -83,9 +77,3 @@ legend <- get_legend(p2)
 
 ggsave("figure2.pdf", plot = p, width = 8, height = 8) #saves only figure
 ggsave("figure2led.pdf", plot = legend, width = 20, height = 20, limitsize = FALSE)# saves only ledgend
-
-#scale_color_gradientn(colours = rainbow(26))
-  #scale_fill_manual()
-  #scale_fill_discrete()
-  #scale_colour_gradient()
-#scale_fill_brewer(palette="Accent")
