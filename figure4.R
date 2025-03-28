9aee73222e4d8d5d17e9bc8992240deb15e3fd3d
Pratimagtm <43498642+Pratimagtm@users.noreply.github.com>
2025-03-28 11:08:39 -0700

    update

--- figure4.R
+++ figure4.R
@@ -3,22 +3,21 @@ library(readxl)
 library(openxlsx)
 library(ggplot2)
 library(tidyr)
-library(wesanderson)
-library(cowplot)
 
-excel_path <-("/home/pratima/Insync/pgautam1@umbc.edu/Google Drive/Cusick Lab/Metagenomic_analysis/Megan7_analysis/megan7_bacteria/figure4/cellenvelope.xlsx")
-#View(excel_path)
+#Excel file includes count for a SEED functional profiling of cell envelope categories for 2 sample types
+#Can be applied to any functional groups in the same excel_path within different sheets
+excel_path <-("../cellenvelope.xlsx")
 
 sheet_data <- read.xlsx(excel_path, sheet = "Sheet1", colNames = TRUE)
-# Data
+# Data includes count data for systems under cell envelope for Sample1 and Sample 2
 data <- data.frame(
   Category <- sheet_data[1],
-  Biofilm <- sheet_data[2],
-  Water <- sheet_data[3]
+  Sample1 <- sheet_data[2],
+  Sample2 <- sheet_data[3]
 )
 View(data)
 data_long <- data %>%
-  gather(key = "Condition", value = "Count", Biofilm, Water)
+  gather(key = "Condition", value = "Count", Sample1, Sample2)
 
 # Calculate total counts per condition (Biofilm, Water)
 total_counts <- data_long %>%
@@ -30,12 +29,6 @@ data_long <- data_long %>%
   left_join(total_counts, by = "Condition") %>%
   mutate(Percentage = Count / Total * 100)
 
-# Order organisms within each condition based on their percentage contribution
-#data_long <- data_long %>%
- # group_by(Condition) %>%
-  #mutate(Category = fct_reorder(Category, Percentage, .desc = FALSE))
-
-View(data_long)
 # Create the bar plot
 p4 <-ggplot(data_long, aes(x = Category, y = Percentage, fill = Condition)) +
   geom_bar(stat = "identity", position = "dodge2") +
@@ -44,9 +37,8 @@ p4 <-ggplot(data_long, aes(x = Category, y = Percentage, fill = Condition)) +
     x = "Cell envelope",
     y = "Number of aligned bases(%)"
   ) +
-  scale_fill_manual(values = c("Biofilm" = "steelblue", "Water" = "#990000")) +
+  scale_fill_manual(values = c("Sample1" = "steelblue", "Sample2" = "#990000")) +
   theme_minimal()+ 
   theme(axis.text.x = element_text(angle=90, vjust=0))
 folder = getwd()
-View(folder)
-ggsave(path =folder, file="figure4.pdf", plot = p4, width = 12, height = 8) #saves only figure
+ggsave(path =folder, file="figure4.pdf", plot = p4, width = 12, height = 8) 
